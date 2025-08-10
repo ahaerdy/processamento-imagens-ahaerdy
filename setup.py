9f7b4aa941e2ee6f0a0b611726d9694c66fb2818
Arthur <arthur.haerdy@gmail.com>
2025-08-10 12:11:43 -0300

    feat: atualiza projeto

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="processamento_imagens",
+    name="processamento-imagens-ahaerdy",
     version="0.0.1",
     author="Arthur Haerdy",
     author_email="arthur.haerdy@gmail.com",
     description="Pacote simples para aplicar transformações e filtros em imagens usando Python e Pillow.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ahaerdy/processamento-imagens",
+    url="https://github.com/ahaerdy/processamento-imagens-ahaerdy",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
